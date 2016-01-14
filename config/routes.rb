85c0463bf606cde3d0d89bddb22810f8fa225a24
Richard Mable <richardbmable@gmail.com>
2016-01-14 16:04:01 -0500

    intital set up complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+
+  devise_scope :user do
+    get "sign_in", to: "devise/sessions#new"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
